23490ca40391b880644dc3440d61b2e1c2d386aa
Shriya Srinivasan <johndoe@example.com>
2023-11-05 02:08:49 -0600

    added

--- pages/home.py
+++ pages/home.py
@@ -2,5 +2,6 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 def display():
-    iframe_src = "https://www.fanniemae.com"
-    components.iframe(iframe_src)
+    with open('abt.html', 'r') as html_file:
+        html_content = html_file.read()
+    components.html(html_content, height=2000, scrolling=True)
